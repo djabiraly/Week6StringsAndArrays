aa1aa551d33aaa8ed57588a49c8ed4c3ba57db7d
Your Name <you@example.com>
2023-04-02 10:45:14 +0300

    Week 6

---
+++ Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿
+string[] fruitBasket = { "kiwi", "mango", "apple", "avocado" };
+
+Console.WriteLine($"There are {fruitBasket.Length} fruit in your Basket.");
+
+//Console.WriteLine(fruitBasket[0]);
+//Console.WriteLine(fruitBasket[1]);
+//Console.WriteLine(fruitBasket[2]);
+//Console.WriteLine(fruitBasket[3]);
+
+//fruitBasket[3] = "plum";
+//fruitBasket[1] = "coconut";
+
+//Console.WriteLine(fruitBasket[0]);
+//Console.WriteLine(fruitBasket[1]);
+//Console.WriteLine(fruitBasket[2]);
+//Console.WriteLine(fruitBasket[3]);
+
+for(int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
+
+fruitBasket[3] = "plum";
+fruitBasket[1] = "coconut";
+
+
+for (int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+String username = "Djabiraly";
+Console.WriteLine(username.Length);
+ Console.WriteLine($"The first latter in your name is {username[0]}.");
+
+Console.WriteLine($"The Last latter in your name is {username[username.Length - 1]}.");
+
+//Console.WriteLine(username[0]);
+//Console.WriteLine(username[1]);
+//Console.WriteLine(username[2]);
+//Console.WriteLine(username[3]);
+//Console.WriteLine(username[4]);
+//Console.WriteLine(username[5]);
+//Console.WriteLine(username[6]);
+//Console.WriteLine(username[7]);
+//Console.WriteLine(username[8]);
+
+////a-counter
+
+//int counter = 0;
+
+//for (int i = 0; i < username.Length; i++)
+//{
+// if (username[i] == 'j')
+//    {
+//        counter++; //counter = counter + 1
+//    }
+
+//}
+
+//Console.WriteLine($"Your name has {counter} j-latters.");
+
+//replace
+//a - 4
+
+string updateUserName = username.Replace('d', '3').Replace('j', '9').Replace('a', '2');
+Console.WriteLine(updateUserName);
